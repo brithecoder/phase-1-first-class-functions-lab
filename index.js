f003c51146ca2a6e7a637684ea6c0d16cf548373
Brianna Gordon <brigordon574@gmail.com>
2021-05-19 17:10:34 -0700

    commit tree 02904aca7158e13fa820dd0c4d9b95e8ff071d54

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+
+const drivers = ['Sally', 'Bob', 'Freddy', 'Claudia'];
+
+function returnFirstTwoDrivers(){
+    const drivers2= [...drivers]
+    return drivers2.slice(0,2)
+}
+
+function returnLastTwoDrivers(){
+    const drivers3=[...drivers]
+     return drivers3.slice(-2)
+}
+
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers];
+
+
+function createFareMultiplier(integer){
+return totalFarePrice => totalFarePrice* integer
+ }
+ 
+ function fareDoubler(fare){
+     return fare * 2;
+ }
+ function fareTripler(fare2){
+     return fare2*3;
+ }
+ function selectDifferentDrivers(driversArray,driversFunctions){
+     return driversFunctions(driversArray)
+ }
+ selectDifferentDrivers(drivers,returnLastTwoDcrivers)
